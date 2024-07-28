c53832546adfa1fd497f1bb6b08508bc84acf361
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-07-28 11:38:26 -0300

    Update HomePage.tsx

--- web/app/home/HomePage.tsx
+++ web/app/home/HomePage.tsx
@@ -58,11 +58,11 @@ export default function HomePage() {
             />
           </div>
         </section>
-        <section className="w-full bg-muted py-12 md:py-24 lg:py-32">
+        <section className="w-full bg-muted py-12 md:py-24 lg:py-32 ">
           <div className="container px-4 md:px-6">
             <div className="flex flex-col items-center justify-center space-y-4 text-center">
               <div className="space-y-2">
-                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl">
+                <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl ">
                   Explore Game Categories
                 </h2>
                 <p className="max-w-[900px] text-muted-foreground md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
@@ -70,7 +70,7 @@ export default function HomePage() {
                 </p>
               </div>
             </div>
-            <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3">
+            <div className="mx-auto grid items-start gap-8 sm:max-w-4xl sm:grid-cols-2 md:gap-12 lg:max-w-5xl lg:grid-cols-3 mt-12">
               <Link
                 href="#"
                 className="group grid gap-1 rounded-lg bg-background p-4 hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground"
